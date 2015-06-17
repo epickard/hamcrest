0ad0a6bde6c547a3922b442b3871d86e7931ec37
epickard <epickard@users.noreply.github.com>
2015-06-17 15:55:17 -0700

    Familiarized myself with some of the Object & Number Hamcrest Matchers

--- Matchers.java
+++ Matchers.java
@@ -1,9 +1,14 @@
-package hamcrest;
+package hamcrest_study;
+
+import java.math.BigDecimal;
 
 import org.junit.*;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasToString;
+import static org.hamcrest.Matchers.closeTo;
+
 
 public class Matchers {
 	
@@ -18,7 +23,7 @@ public class Matchers {
 	//For custom descriptions. Useful when expecting not null, tells you what was supposed to be not null :)
 	public void testDescribedAs() {
 		String a = null;
-		assertThat(a, describedAs("greeting", is(not(nullValue()))));	
+		assertThat(a, describedAs("greeting", is(nullValue())));	
 	}
 	
 	
@@ -48,5 +53,53 @@ public class Matchers {
 		String a = "hello";
 		String b = "hello";
 		assertThat(a, is(equalTo(b)));
+	}
+	
+	@Test
+	//Assertion requires Object to be tested and the expected contents of the toString() method. Expected contents 
+	//must exactly match results returned from the toString() 
+	public void testHasToString() {
+		Foo myFoo = new Foo();
+		assertThat(myFoo, hasToString("hello"));
+	}
+	
+	@Test
+	public void testInstanceOf() {
+		assertThat(new Foo(), is(instanceOf(Object.class)));
+	}
+	
+	@Test
+	public void testNotNullValue() {
+		Foo myFoo = new Foo();
+		assertThat(myFoo, is(notNullValue()));
+	}
+	
+	@Test
+	public void testNullValue() {
+		String myFoo = null;
+		assertThat(myFoo, is(nullValue()));
+	}
+	
+	@Test
+	//Failure message shows "differed by" difference outside the range, not total difference
+	public void testCloseToWithDouble() {
+		Double d1 = 6.00; //For easy demo of error condition change to 7.00
+		Double d2 = 4.00;
+		Double d3 = 2.00;
+		assertThat(d1, is(closeTo(d2, d3)));
+	}
+	
+	@Test
+	//Failure message shows "differed by" difference outside the range, not total difference
+	public void testCloseToWithBigDecimal() {
+		BigDecimal bd1 = new BigDecimal(6.00); //For easy demo of error condition change to 7.00
+		BigDecimal bd2 = new BigDecimal(4.00);
+		BigDecimal bd3 = new BigDecimal(2.00);
+		assertThat(bd1, is(closeTo(bd2, bd3)));
+	}
+	
+	@Test
+	public void testGreaterThan() {
+		
 	}
 }
